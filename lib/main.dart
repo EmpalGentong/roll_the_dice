c85fb3319478ae311b918e15635af20aad759ee0
Empalgentong <saihan507@gmail.com>
2023-07-06 20:14:39 +0700

    update button

--- lib/main.dart
+++ lib/main.dart
@@ -49,8 +49,8 @@ class _MyHomePageState extends State<MyHomePage> {
       body: BackgroundColor(counter: _counter),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        tooltip: 'roll dice',
+        child: Text('ROLL'),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
